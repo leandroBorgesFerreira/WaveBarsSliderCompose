52df8b054285e15923300a1f706aa6766c36bd18
Leandro Borges Ferreira <lehen01@gmail.com>
2023-04-01 18:34:50 +0200

    Moving tracker at center position

--- app/src/main/java/br/com/leandroferreira/wavebarsslidercompose/MainActivity.kt
+++ app/src/main/java/br/com/leandroferreira/wavebarsslidercompose/MainActivity.kt
@@ -3,12 +3,20 @@ package br.com.leandroferreira.wavebarsslidercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import br.com.leandroferreira.wavebarsslidercompose.ui.theme.WaveBarsSliderComposeTheme
 import br.com.leandroferreira.wavebarsslidercompose.ui.view.AudioWaveView
@@ -21,7 +29,7 @@ class MainActivity : ComponentActivity() {
         // A surface container using the 'background' color from the theme
         Surface(
           modifier = Modifier
-            .height(50.dp)
+            .height(150.dp)
             .width(250.dp),
           color = MaterialTheme.colors.background
         ) {
@@ -30,10 +38,20 @@ class MainActivity : ComponentActivity() {
               repeat(30) { i ->
                 add(i / 30F)
               }
-            }, modifier = Modifier
+            },
+            modifier = Modifier
               .height(50.dp)
               .width(200.dp)
               .padding(horizontal = 30.dp, 6.dp),
+            trackerFunc = {
+              Box(
+                modifier = Modifier
+                  .width(5.dp)
+                  .height(5.dp)
+                  .clip(RectangleShape)
+                  .background(Color(0x5500FF00))
+              )
+            }
           )
         }
       }
--- app/src/main/java/br/com/leandroferreira/wavebarsslidercompose/ui/view/AudioWavesSeekBar.kt
+++ app/src/main/java/br/com/leandroferreira/wavebarsslidercompose/ui/view/AudioWavesSeekBar.kt
@@ -1,12 +1,11 @@
 package br.com.leandroferreira.wavebarsslidercompose.ui.view
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.awaitHorizontalDragOrCancellation
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,15 +13,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.leandroferreira.wavebarsslidercompose.ui.theme.WaveBarsSliderComposeTheme
 import java.lang.Float.max
 
+private const val DEFAULT_TRACKER_HEIGHT_DP = 60
+private const val DEFAULT_TRACKER_WIDTH_DP = 20
 
 @Composable
 fun AudioWaveView(
@@ -30,20 +35,20 @@ fun AudioWaveView(
   modifier: Modifier = Modifier,
   colorLeft: Color = Color.Blue,
   colorRight: Color = Color.Gray,
+  trackerFunc: @Composable () -> Unit
 ) {
 
   var progress by remember { mutableStateOf(0F) }
   var seekWidth: Float? = null
   val barSpacing = 0.4
 
+
   Canvas(modifier = modifier.pointerInput(Unit) {
     detectDragGestures { change, _ ->
-      progress = change.position.x * 100 / (seekWidth ?: 10F)
+      progress = change.position.x * 100F / (seekWidth ?: 10F)
     }
   }) {
-    if (seekWidth == null) {
-      seekWidth = size.width
-    }
+    seekWidth = size.width
 
     val totalSpaceWidth = size.width * barSpacing
     val totalBarWidth = size.width - totalSpaceWidth
@@ -51,6 +56,8 @@ fun AudioWaveView(
     val barWidth = totalBarWidth / waveBars.size
     val spaceWidth = totalSpaceWidth / waveBars.size
     val progressToWidth = progress / 100F * size.width
+    val trackerHeight = DEFAULT_TRACKER_HEIGHT_DP
+    val trackerWidth = DEFAULT_TRACKER_WIDTH_DP
 
     repeat(waveBars.size) { i ->
       val barHeight = max(size.height * waveBars[i], 3.dp.toPx())
@@ -69,7 +76,25 @@ fun AudioWaveView(
         cornerRadius = CornerRadius(50F, 50F)
       )
     }
+
+    drawRoundRect(
+      color = Color.Red,
+      topLeft = Offset(
+        0F - trackerWidth.dp.toPx() / 2 + progressToWidth,
+        (size.height / 2) - trackerHeight.dp.toPx() / 2
+      ),
+      size = Size(trackerWidth.dp.toPx(), trackerHeight.dp.toPx()),
+      cornerRadius = CornerRadius(10F, 10F)
+    )
   }
+
+//  Layout(content = trackerFunc, modifier = modifier) { measurables, constraints ->
+//    val trackerPlaceable = measurables.first().measure(constraints)
+//
+//    layout(constraints.maxWidth, constraints.maxHeight) {
+//      trackerPlaceable.place(0, 0)
+//    }
+//  }
 }
 
 @Preview(showBackground = true)
@@ -85,6 +110,14 @@ fun DefaultPreview() {
       modifier = Modifier
         .height(70.dp)
         .width(250.dp),
+      trackerFunc = {
+        Box(
+          modifier = Modifier
+            .size(40.dp)
+            .clip(RectangleShape)
+            .background(Color.Red)
+        )
+      }
     )
   }
 }
