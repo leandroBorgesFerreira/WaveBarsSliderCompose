41293b216748b23ade265f046d07c9002aeb1ef2
Leandro Borges Ferreira <lehen01@gmail.com>
2023-04-01 19:13:51 +0200

    Change tracker

--- app/src/main/java/br/com/leandroferreira/wavebarsslidercompose/ui/view/AudioWavesSeekBar.kt
+++ app/src/main/java/br/com/leandroferreira/wavebarsslidercompose/ui/view/AudioWavesSeekBar.kt
@@ -1,11 +1,8 @@
 package br.com.leandroferreira.wavebarsslidercompose.ui.view
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,13 +10,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +24,7 @@ import java.lang.Float.max
 import java.lang.Float.min
 
 private const val DEFAULT_TRACKER_HEIGHT_DP = 60
-private const val DEFAULT_TRACKER_WIDTH_DP = 8
+private const val DEFAULT_TRACKER_WIDTH = 4
 
 @Composable
 fun AudioWaveView(
@@ -36,18 +32,43 @@ fun AudioWaveView(
   modifier: Modifier = Modifier,
   colorLeft: Color = Color.Blue,
   colorRight: Color = Color.Gray,
-  trackerFunc: @Composable () -> Unit
+  trackerDraw: DrawScope.(Float, Size) -> Unit = { progressWidth, trackerSize ->
+    drawRoundRect(
+      color = Color.Red,
+      topLeft = Offset(
+        min(
+          max(progressWidth - trackerSize.width / 2, 0F),
+          size.width - trackerSize.width
+        ),
+        0F
+      ),
+      size = trackerSize,
+      cornerRadius = CornerRadius(10F, 10F)
+    )
+  } ,
 ) {
 
   var progress by remember { mutableStateOf(0F) }
+  var isDragging by remember { mutableStateOf(false) }
+
   var seekWidth: Float? = null
   val barSpacing = 0.4
 
-
   Canvas(modifier = modifier.pointerInput(Unit) {
-    detectDragGestures { change, _ ->
-      progress = change.position.x * 100F / (seekWidth ?: 10F)
-    }
+    detectHorizontalDragGestures(
+      onDragStart = {
+        isDragging = true
+      },
+      onHorizontalDrag = { change, _ ->
+        progress = change.position.x * 100F / (seekWidth ?: 10F)
+      },
+      onDragCancel = {
+        isDragging = false
+      },
+      onDragEnd = {
+        isDragging = false
+      }
+    )
   }) {
     seekWidth = size.width
 
@@ -58,7 +79,8 @@ fun AudioWaveView(
     val spaceWidth = totalSpaceWidth / waveBars.size
     val progressToWidth = progress / 100F * size.width
     val trackerHeight = DEFAULT_TRACKER_HEIGHT_DP
-    val trackerWidth = DEFAULT_TRACKER_WIDTH_DP
+
+    val trackerWidth = DEFAULT_TRACKER_WIDTH.dp.toPx()
 
     repeat(waveBars.size) { i ->
       val barHeight = max(size.height * waveBars[i], 3.dp.toPx())
@@ -78,27 +100,8 @@ fun AudioWaveView(
       )
     }
 
-    drawRoundRect(
-      color = Color.Red,
-      topLeft = Offset(
-        min(
-          max(progressToWidth - trackerWidth.dp.toPx() / 2, 0F),
-          size.width - trackerWidth.dp.toPx()
-        ),
-        0F
-      ),
-      size = Size(trackerWidth.dp.toPx(), size.height),
-      cornerRadius = CornerRadius(10F, 10F)
-    )
+    trackerDraw(this, progressToWidth, Size(trackerWidth.dp.toPx(), size.height))
   }
-
-//  Layout(content = trackerFunc, modifier = modifier) { measurables, constraints ->
-//    val trackerPlaceable = measurables.first().measure(constraints)
-//
-//    layout(constraints.maxWidth, constraints.maxHeight) {
-//      trackerPlaceable.place(0, 0)
-//    }
-//  }
 }
 
 @Preview(showBackground = true)
@@ -114,14 +117,6 @@ fun DefaultPreview() {
       modifier = Modifier
         .height(70.dp)
         .width(250.dp),
-      trackerFunc = {
-        Box(
-          modifier = Modifier
-            .size(40.dp)
-            .clip(RectangleShape)
-            .background(Color.Red)
-        )
-      }
     )
   }
 }
